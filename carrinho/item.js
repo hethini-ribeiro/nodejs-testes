e52bdae30062888c31e2d031632cffd489bc577e
Hethini Ribeiro <hethini.ribeiro@gmail.com>
2022-11-27 14:44:51 -0200

    Adding tests

---
+++ carrinho/item.js
@@ -0,0 +1,12 @@
+class Item {
+    constructor(nome, valor, quantidade) {
+      this.nome = nome;
+      this.valor = valor;
+      this.quantidade = quantidade;
+    }
+  
+    pegaValorTotalItem() {
+      return this.quantidade * this.valor;
+    }
+  }
+  export default Item;
